4a32cf57eb6383c5c91fb7e96033f487ec4ea6ff
DESKTOP-DJJC42L\vanlo <vanlong11x1@gmail.com>
2025-12-20 16:12:18 +0700

    fix domain

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['lifennew.com', 'www.lifennew.com']
+    allowedHosts: ['caranddriverenthusiast.com', 'www.caranddriverenthusiast.com']
   }
 });
